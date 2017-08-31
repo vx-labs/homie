fcd2fd7d50a0e34fbaa9a245b805b904df11d91d
Julien BONACHERA <jbonachera@xebia.fr>
2017-08-31 10:23:39 +0200

    add property interface and struct

---
+++ property.go
@@ -0,0 +1,65 @@
+package homie
+
+type Property interface {
+	Name() string
+	Value() string
+	Set(value string)
+	Settable() bool
+	Unit() string
+	Datatype() string
+	Format() string
+	Publish() error
+}
+type publishFunc func(property string, value string)
+type property struct {
+	name     string
+	value    string
+	settable bool
+	unit     string
+	datatype string
+	format   string
+	publish  publishFunc
+}
+
+func (p *property) Name() string {
+	return p.name
+}
+func (p *property) Value() string {
+	return p.value
+}
+func (p *property) Set(value string) {
+	p.value = value
+	p.publish(p.name, p.value)
+}
+func (p *property) Settable() bool {
+	return p.settable
+}
+func (p *property) Unit() string {
+	return p.unit
+}
+func (p *property) Datatype() string {
+	return p.datatype
+}
+func (p *property) Format() string {
+	return p.format
+}
+func (p *property) Publish() error {
+	p.publish(p.name, p.value)
+	p.publish(p.name+"/$settable", "false")
+	p.publish(p.name+"/$unit", p.unit)
+	p.publish(p.name+"/$datatype", p.datatype)
+	p.publish(p.name+"/$name", p.name)
+	p.publish(p.name+"/$format", p.format)
+	return nil
+}
+
+func NewProperty(name string, unit string, datatype string, format string, cb publishFunc) Property {
+	return &property{
+		name:     name,
+		settable: false,
+		unit:     unit,
+		datatype: datatype,
+		format:   format,
+		publish:  cb,
+	}
+}
